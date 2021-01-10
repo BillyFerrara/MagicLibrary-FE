7654bf2ddd31341514fd7a0d3f12a34f30fc7819
Billy F <willfira717@gmail.com>
2021-01-09 19:53:39 -0500

    Refactoring into OOJ

--- src/index.js
+++ src/index.js
@@ -2,29 +2,7 @@ const spellbookURL = 'http://localhost:3000/spellbooks'
 const spellURL = 'http://localhost:3000/spells'
 
 //console.log('index.js')
-function fetchSpellbooks()
-{
-   fetch(spellbookURL)
-   .then(res => res.json())
-   .then(sb => sb.forEach(spellbook => {
-      let newSpellbook = new Spellbook(spellbook.title)
-   console.log(newSpellbook)
-   renderSpellbook(sb)
-   })) 
-}
 
-function renderSpellbook(sb){
-   for ( let s of sb) {
-
-   const sbUL = document.getElementById('spellbook-list')
-   
-   const sbLI = document.createElement('li')
-   const p = document.createElement('p')
-
-   p.innerHTML = s.title
-   sbLI.append(p)
-   sbUL.append(sbLI)
-   }
-}
 
 fetchSpellbooks();
+renderSpell();
--- src/spellbook.js
+++ src/spellbook.js
@@ -2,6 +2,43 @@ class Spellbook{
     constructor(title) {
         this.title = title
     }
+    function fetchSpellbooks()
+{
+   fetch(spellbookURL)
+   .then(res => res.json())
+   .then(sb => sb.forEach(spellbook => {
+      let newSpellbook = new Spellbook(spellbook.title)
+   console.log(newSpellbook)
+   renderSpellbook(sb)
+   })) 
 }
+
+function renderSpellbook(sb){
+   for ( let s of sb) {
+
+   const sbUL = document.getElementById('spellbook-list')
+   
+   const sbLI = document.createElement('li')
+   const p = document.createElement('p')
+   const spellUL = document.createElement('ul')
+   const spellP = document.createElement('p')
+      // needs a const for a new spell form 
+
+    s.spells.forEach(spell => {
+      let newSpell = new Spell(spell)
+      debugger
+     })  
+     
+   spellUL.append(spellP)
+
+   p.innerHTML = s.title
+   sbLI.append(p)
+   sbUL.append(sbLI, spellUL)
+
+      console.log(s.spells)
+   }
+   
+}
+
 
 //console.log('spellbook')
